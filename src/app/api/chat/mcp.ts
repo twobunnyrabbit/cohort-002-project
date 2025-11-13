65b63a1785bfc03bb1679db5ddc7ee40e039104c
Matt Pocock <mattpocockvoice@gmail.com>
2025-11-13 11:42:57 +0000

    10.01.02 Removed unwanted tools

--- src/app/api/chat/mcp.ts
+++ src/app/api/chat/mcp.ts
@@ -1,4 +1,16 @@
 import { experimental_createMCPClient } from "@ai-sdk/mcp";
+import { ToolSet } from "ai";
+
+const deleteUnwantedTools = (tools: ToolSet) => {
+  if ("add_tools" in tools) {
+    delete tools.add_tools;
+  }
+  if ("edit_tools" in tools) {
+    delete tools.edit_tools;
+  }
+
+  return tools;
+};
 
 export const getMCPTools = async () => {
   const httpClient = await experimental_createMCPClient({
@@ -10,5 +22,9 @@ export const getMCPTools = async () => {
 
   const tools = await httpClient.tools();
 
-  return tools;
+  for (const tool of Object.keys(tools)) {
+    console.log(tool, (tools[tool].inputSchema as any).jsonSchema);
+  }
+
+  return deleteUnwantedTools(tools);
 };
